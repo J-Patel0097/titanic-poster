957922c46c9b2f121aeadbbbaf978d801a49928d
J-Patel0097 <jaypatel37650@gmail.com>
2024-09-06 01:14:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,52 +1,32 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
 import joblib
 
+model = joblib.load('titanic_model.pkl')
 
-clf = joblib.load(open("titanic_model.pkl","rb"))
+st.title('Titanic Survival Prediction')
 
-def predict(data):
-    clf = joblib.load(open("titanic_model.pkls","rb"))
-    return clf.predict(data)
+st.write('Please provide the following details:')
 
-st.title("Titanic Passenger Survival Prediction")
-st.markdown("This model predicts whether a Titanic passenger survived or not based on input features.")
+pclass = st.selectbox('Passenger Class', [1, 2, 3])
+sex = st.selectbox('Sex', ['male', 'female'])
+age = st.slider('Age', 0, 80, 25)
+fare = st.slider("Ticket Fare", 0.0, 10000.0, 50.0)
+embarked = st.selectbox('Port of Embarkation', ['Southampton', 'Cherbourg', 'Queenstown'])
 
+sex = 0 if sex == 'Male' else 1
+embarked = {'Southampton': 0, 'Cherbourg': 1, 'Queenstown': 2}[embarked]
 
-st.header("Traveler Details")
+features = pd.DataFrame({
+ 'pclass': [pclass],
+ 'sex': [sex],
+ 'age': [age],
+ 'fare': [fare],
+ 'embarked': [embarked]
+})
 
-
-col1, col2 = st.columns(2)
-
-
-with col1:
-    age = st.slider("Age", 1, 100, 25)
-    fare = st.slider("Ticket Fare", 0.0, 10000.0, 50.0)
-
-
-with col2:
-    pc = st.selectbox("Passenger Class", ["First Class", "Second Class", "Third Class"])
-    bc = st.selectbox("Boarding From", ["Cherbourg", "Queenstown", "Southampton"])
-
-# Convert passenger class and boarding location to numerical values
-# For passenger class
-if pc == "First Class":
-    pc = 1
-elif pc == "Second Class":
-    pc = 2
-else:
-    pc = 3
-
-# For boarding location
-if bc == "Cherbourg":
-    boarding = 1
-elif bc == "Queenstown":
-    boarding = 2
-else:
-    boarding = 3
-
-# Prediction button
-if st.button("Predict Survival"):
-    result = clf.predict(np.array([[age,fare,pc,boarding,1,1,1]]))
-    st.text(result[0])
+if st.button('Predict'):
+    prediction = model.predict(features)[pclass,sex,age,fare,embarked]
+    result = 'Survived' if prediction == 1 else 'Not Survived'
+    st.write(f'The model predicts: {result}')
+st.write("Developed by Jay Patel")
